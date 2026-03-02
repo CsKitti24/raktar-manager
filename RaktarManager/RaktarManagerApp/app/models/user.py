4de6dacceb51cb32473f050dceb1a5190edead2f
Balla Zsófia <zsofiaballa7@gmail.com>
2026-03-02 23:17:27 +0100

    alap fájlok

---
+++ RaktarManager/RaktarManagerApp/app/models/user.py
@@ -0,0 +1,11 @@
+from app.extensions import db
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from typing import List
+from sqlalchemy.types import String
+
+#ez nincs kész csak leteszteltem
+class User(db.Model):
+    __tablename__ = 'users'
+    id: Mapped[int] = mapped_column(primary_key=True)
+    def __repr__(self):
+        return f'<User {self.username}>'
