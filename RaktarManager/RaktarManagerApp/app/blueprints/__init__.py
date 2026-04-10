3dde556ab9d7e0633219b113f7df533c912e5183
Balla Zsófia <zsofiaballa7@gmail.com>
2026-04-10 18:23:39 +0200

    jwt token hozzáadva, userbp-n pár változtatás de még nem végleges

--- RaktarManager/RaktarManagerApp/app/blueprints/__init__.py
+++ RaktarManager/RaktarManagerApp/app/blueprints/__init__.py
@@ -2,10 +2,40 @@ from apiflask import APIBlueprint
 bp = APIBlueprint('main', __name__, tag="main")
 from app.blueprints import bp
 from app.models import *
+from apiflask import HTTPError
+from app.extensions import auth
+from flask import current_app
+from datetime import datetime
+from authlib.jose import jwt
 
 @bp.route('/')
 def index():
     return 'This is The Main Blueprint'
+
+@auth.verify_token
+def verify_token(token):
+    try:
+        data = jwt.decode(
+            token.encode('ascii'),
+           
+            current_app.config['SECRET_KEY'],
+        )
+        if data["exp"] < int(datetime.now().timestamp()):
+            return None
+        return data
+    except Exception as ex:
+        return None
+
+def role_required(roles):
+    def wrapper(fn):
+        def decorated_function(*args, **kwargs):
+            user_roles = [item["name"] for item in auth.current_user.get("roles")]
+            for role in roles:
+                if role in user_roles:
+                    return fn(*args, **kwargs)        
+            raise HTTPError(message="Access denied", status_code=403)
+        return decorated_function
+    return wrapper
 
 #Registrate blueprints here...
 from app.blueprints.user import bp as bp_user
