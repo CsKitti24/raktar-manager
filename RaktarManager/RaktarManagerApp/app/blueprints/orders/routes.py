24b302697eea7ebaa1e0a20364898ca70f1caba5
Balla Zsófia <zsofiaballa7@gmail.com>
2026-04-13 19:17:39 +0200

    orders bp

--- RaktarManager/RaktarManagerApp/app/blueprints/orders/routes.py
+++ RaktarManager/RaktarManagerApp/app/blueprints/orders/routes.py
@@ -1,8 +1,99 @@
-from flask import jsonify
+﻿from flask import jsonify
 from app.blueprints.orders import bp
 from apiflask import HTTPError
+from app.blueprints.orders.schemas import OrderRequestSchema, OrderResponseSchema, OrderUpdateRequestSchema,OrderStatusRequestSchema, OrderAssignUserSchema, OrderAssignLocationSchema
+from app.blueprints.orders.service import OrderService
+from app.extensions import auth
+from app.blueprints import role_required
 
 @bp.route("/")
 
 def index():
     return 'This is The Orders Blueprint'
+
+#Megrendelések listázása
+@bp.get('/get-orders')
+@bp.doc(tags=["orders"])
+@bp.auth_required(auth)
+@bp.output(OrderResponseSchema(many=True))
+def get_orders():
+    success, response = OrderService.get_orders(auth.current_user)
+    if success:
+        return response, 200
+    raise HTTPError(message=response, status_code=400)
+
+#Megrendelés részleteinek lekérése ID alapján   ✔
+@bp.get('/<int:order_id>')
+@bp.doc(tags=["orders"])
+@bp.auth_required(auth)
+@bp.output(OrderResponseSchema)
+def get_order(order_id):
+    success, response = OrderService.get_order_by_id(order_id, auth.current_user)
+    if success: 
+        return response, 200
+    raise HTTPError(message=response, status_code=404)
+
+#Megrendelés létrehozása   ✔
+@bp.post('/create')
+@bp.doc(tags=["orders"])
+@bp.auth_required(auth)
+@role_required(["Orderer", "Admin"])
+@bp.input(OrderRequestSchema, location="json")
+@bp.output(OrderResponseSchema)
+def create_order(json_data):
+    success, response = OrderService.create_order(json_data, auth.current_user.get("user_id"))
+    if success:
+        return response, 200 
+    raise HTTPError(message=response, status_code=400)
+
+#Megrendelés módosítása     ✔
+@bp.put('/<int:order_id>/update')
+@bp.doc(tags=["orders"])
+@bp.auth_required(auth)
+@role_required(["Orderer", "Admin"])
+@bp.input(OrderUpdateRequestSchema, location="json")
+@bp.output(OrderResponseSchema)
+def update_order(order_id, json_data):
+    success, response = OrderService.update_order(order_id, json_data, auth.current_user.get("user_id"))
+    if success: 
+        return response, 200
+    raise HTTPError(message=response, status_code=400)
+
+#Állapot módosítása   ✔
+@bp.put('/<int:order_id>/status')
+@bp.doc(tags=["orders"])
+@bp.auth_required(auth)
+@role_required(["Warehouseman", "Carrier", "Admin"])
+@bp.input(OrderStatusRequestSchema, location="json")
+@bp.output(OrderResponseSchema)
+def update_status(order_id, json_data):
+    success, response = OrderService.update_status(order_id, json_data)
+    if success: 
+        return response, 200
+    raise HTTPError(message=response, status_code=400)
+
+#Beszállító hozzárendelése   ✔
+@bp.put('/<int:order_id>/assign-supplier')
+@bp.doc(tags=["orders"])
+@bp.auth_required(auth)
+@role_required(["Warehouseman", "Admin"])
+@bp.input(OrderAssignUserSchema, location="json")
+@bp.output(OrderResponseSchema)
+def assign_supplier(order_id, json_data):
+    success, response = OrderService.assign_user(order_id, json_data['user_id'], 'Supplier', auth.current_user.get("user_id"))
+    if success:
+        return response, 200
+    raise HTTPError(message=response, status_code=400)
+
+#Fuvarozó hozzárendelése   ✔
+@bp.put('/<int:order_id>/assign-carrier')
+@bp.doc(tags=["orders"])
+@bp.auth_required(auth)
+@role_required(["Warehouseman", "Admin"])
+@bp.input(OrderAssignUserSchema, location="json")
+@bp.output(OrderResponseSchema)
+def assign_carrier(order_id, json_data):
+    success, response = OrderService.assign_user(order_id, json_data['user_id'], 'Carrier', auth.current_user.get("user_id"))
+    if success:
+        return response, 200
+    raise HTTPError(message=response, status_code=400)
--- RaktarManager/RaktarManagerApp/app/blueprints/orders/schemas.py
+++ RaktarManager/RaktarManagerApp/app/blueprints/orders/schemas.py
@@ -1,1 +1,50 @@
+from marshmallow import Schema, fields
+from apiflask.fields import String, Nested, Integer, List, DateTime
 
+
+class OrderItemSchema(Schema):
+    id = fields.Integer(dump_only = True)
+    order_id = fields.Integer(dump_only = True)
+    product_id = fields.Integer()
+    quantity = fields.Integer()
+
+class OrderRequestSchema(Schema):
+    address_id = fields.Integer()
+    comment = fields.String(allow_none = True)
+    items = fields.List(fields.Nested(OrderItemSchema))
+
+class AdressSchema(Schema):
+    country = fields.String()
+    city = fields.String()
+    street = fields.String()
+    postal_code = fields.String()
+  
+class OrderResponseSchema(Schema):
+    id = fields.Integer()
+    order_number = fields.String()
+    orderer_id = fields.Integer()
+    supplier_id = fields.Integer(allow_none=True)
+    carrier_id = fields.Integer(allow_none=True)
+    warehouse_user_id = fields.Integer(allow_none=True)
+    status = fields.String()
+    comment = fields.String()
+    total_amount = fields.Integer()
+    is_locked = fields.Integer()
+    created_at = fields.DateTime()
+    updated_at = fields.DateTime()
+    address = fields.Nested(AdressSchema)
+    items = fields.List(fields.Nested(OrderItemSchema))
+
+class OrderUpdateRequestSchema(Schema):
+    address_id = fields.Integer()
+    comment = fields.String(allow_none = True)
+    items = fields.List(fields.Nested(OrderItemSchema))
+
+class OrderStatusRequestSchema(Schema):
+    status = fields.String()
+
+class OrderAssignUserSchema(Schema):
+        user_id = fields.Integer()
+
+class OrderAssignLocationSchema(Schema):
+    location_id = fields.Integer()
